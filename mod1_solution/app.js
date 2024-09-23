c574fbdc94158ad91c5b0920add7c60136ee46a6
Gabriele Falloon <gabrielefalloon@Gabrieles-MBP.lan>
2024-09-23 11:00:52 -0500

    removed console logs

--- mod1_solution/app.js
+++ mod1_solution/app.js
@@ -13,20 +13,19 @@
 
         $scope.displayMsg = function () {
             var items = calcNumItems($scope.userItems);
-            console.log(items)
 
             if (items == 0) {
                 $scope.message = 'Please enter data first'
             } else if (items > 3) {
                 $scope.message = 'Too Much!'
-            } else if (items <= 3 ) {
+            } else {
                 $scope.message = 'Enjoy!'
             }
         };
 
         function calcNumItems(str) {
-            console.log(str)
             var totalItems = 0;
+            
             if (str == '') {
                 return totalItems
             } else {
